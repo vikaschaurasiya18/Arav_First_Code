289af952af6877cf160d54370c4db4aedcca1301
Vikas Chaurasiya <vikaschaurasiya98@gmail.com>
2026-07-12 12:28:33 +0530

    New File

---
+++ Project.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter your name: ");
+        String studentName = scanner.nextLine();
+
+        System.out.print("Enter your age: ");
+        int studentAge = scanner.nextInt();
+
+        System.out.println("Welcome, " + studentName);
+        System.out.println("Your age is, " + studentAge);
+
+    }
+}
