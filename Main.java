eecf9c286f0da53ff9ee89d8a1ef3b651fb9e967
Vikas Chaurasiya <vikaschaurasiya98@gmail.com>
2026-07-12 12:23:27 +0530

    New File

---
+++ Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args){
+        String Name, Course;
+        int Class, Age;
+        Name = "Vikas Chaurasiya";
+        Class = 11;
+        Age = 23;
+        Course = "PCM";
+        System.out.println(Name);
+        System.out.println(Class);
+        System.out.println(Age);
+        System.out.println(Course);
+    }
+}
